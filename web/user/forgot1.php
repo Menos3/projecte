6d8dbef40f613c49d6002c98f2ebc2a0118a522b
Sergio Melado Alonso <ser_melado@hotmail.com>
2021-11-12 19:21:24 +0100

    forgot1 forgot2 con action creados

---
+++ web/user/forgot1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar Contrasenya</title>
+</head>
+<body>
+    <form method = "post">
+        <h1>Recuperar Contrasenya</h1>
+        <p>Escriu el correu electrónic amb el que t'has registrat</p>
+        <input type = "text" id = "tfEmail"/>
+        <br><br>
+        <button id = "btCancelar">Cancelar</button>
+        <button id = "btEnviarCorreu">Recuperar Contrasenya</button>
+    </form>
+</body>
+</html>
---
+++ web/user/forgot2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar contrasenya</title>
+</head>
+<body>
+    <form method = "post">
+        <h1>Canviar Contrasenya</h1>
+        <p>Escriu la nova contraseya</p>
+        <input type = "text" id = "tfNovaContrasenya"/>
+        <p>Torna a escriure la nova contrasenya</p>
+        <input type = "text" id = "tfRepetirContrasenya"/>
+        <br><br>
+        <button id = "btCanviarContrasenya">Canviar Contrasenya</button>
+    </form>
+</body>
+</html>
