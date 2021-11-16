2f10034eb207dc8718699d3d571954ab537b7484
Sergio Melado Alonso <ser_melado@hotmail.com>
2021-11-16 19:40:54 +0100

    Arreglo de vulnerabilidad

--- config/database.php
+++ config/database.php
@@ -3,10 +3,10 @@
 return [
    "driver"    => "mysql",
    "host"      => "alumnes.insjoaquimmir.cat",
-   "port"      => 9316,
-   "database"  => "demo",
-   "user"      => "demo",
-   "password"  => "VGz739ghGmQU",
+   "port"      => 8080,
+   "database"  => "anonimo",
+   "user"      => "anonimo",
+   "password"  => "anonimo",
    "options"   => [
        PDO::MYSQL_ATTR_SSL_KEY                => __DIR__ . '/client-key.pem',
        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
