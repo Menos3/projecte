28e5012b09384ade29c4974972e3d93f35d1d600
cristina <cristinaaran89@gmail.com>
2021-11-19 21:08:26 +0100

    clases

---
+++ web/_commons/js/clases/Usuari.js
@@ -0,0 +1,13 @@
+export class Usuari {
+    constructor(user_id, username, role_id) {
+        this.user_id = user_id;
+        this.username = username;
+        this.role_id = role_id;
+    }
+
+
+
+
+
+
+}
---
+++ web/tickets/src/js/Tickets.js
@@ -0,0 +1,12 @@
+export class Tickets {
+    constructor(id, titulo, descripcion, creado, actualizado) {
+        this.id = id;
+        this.titulo = titulo;
+        this.decripcion = descripcion;
+        this.creado = creado;
+        this.actualizado = actualizado;
+
+
+    }
+
+}
