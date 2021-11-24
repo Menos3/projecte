428cd2d10f281cc95773d79062d2864931aff06b
Sergio Melado Alonso <ser_melado@hotmail.com>
2021-11-24 17:41:16 +0100

    database conf

--- config/database.php
+++ config/database.php
@@ -3,10 +3,10 @@
 return [
    "driver"    => "mysql",
    "host"      => "alumnes.insjoaquimmir.cat",
-   "port"      => 8080,
-   "database"  => "anonimo",
-   "user"      => "anonimo",
-   "password"  => "anonimo",
+   "port"      => 9316,
+   "database"  => "2daw.equip02",
+   "user"      => "2daw.equip02",
+   "password"  => "bA*HN3yH",
    "options"   => [
        PDO::MYSQL_ATTR_SSL_KEY                => __DIR__ . '/client-key.pem',
        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
