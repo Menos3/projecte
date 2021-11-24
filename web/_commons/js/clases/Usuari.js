a9de6f01d7aa84a2110c79255ba3bf425c53db9a
cristina <cristinaaran89@gmail.com>
2021-11-24 17:15:12 +0100

    m6

--- web/_commons/js/clases/Usuari.js
+++ web/_commons/js/clases/Usuari.js
@@ -1,13 +1,11 @@
 export class Usuari {
+    user_id;
+    username;
+    role_id;
     constructor(user_id, username, role_id) {
         this.user_id = user_id;
         this.username = username;
         this.role_id = role_id;
     }
-
-
-
-
-
 
 }
---
+++ web/tickets/src/js/formulario.js
@@ -0,0 +1,9 @@
+const formulari = document.getElementById("acept");
+const ticketName = document.getElementById("name");
+const ticketDesc = document.getElementById("desc");
+const assignation = document.getElementById("assignation");
+const ticketAssets = document.getElementById("assets");
+const ticketAuthor = document.getElementById("author");
+
+tickets
+//preliminares de las clases.
