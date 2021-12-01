4a5102bf80d1bd4b5cf1db95bc53009def6a1f15
cristina <cristinaaran89@gmail.com>
2021-12-01 17:12:02 +0100

    acabat

--- web/tickets/src/js/Tickets.js
+++ web/tickets/src/js/Tickets.js
@@ -1,14 +1,16 @@
 export class Tickets {
-    constructor(id, titulo, descripcion, assigenedId, assetId) {
+    constructor(props) {
+        //que solo reciba un objecto en vez de los 5 argumentos
+        // destructuracion
+        const { id, titulo, descripcion, assignedId, assetId } = props;
         this.id = id;
         this.titulo = titulo;
-        this.decripcion = descripcion;
-        this.assigenedId = assigenedId;
-        this.assigenedId = assetId;
-        // this.created = this.getDate();
+        this.descripcion = descripcion;
+        this.assignedId = assignedId;
+        this.assetId = assetId;
+        //esto lo coge del metodo del constructor: nose pasa por props.
+        this.created = this.getDate();
         // this.actualizado = this.getDate();
-
-
     }
     getDate() {
         let date = new Date();
@@ -29,14 +31,16 @@ export class ListTickets {
         this.setLocalStorage();
     }
     setLocalStorage() {
-        localStorage.setItem('tickets', this.tickets);
+        localStorage.setItem('tickets', JSON.stringify(this.tickets));
     }
     getLocalStorage() {
         this.tickets = (localStorage.getItem('tickets')) ?
             JSON.parse(localStorage.getItem('tickets')) : [];
+        return this.tickets;
     }
     getLastId() {
-        id = this.ticketsat(-1).id;
+        let id = this.tickets.length > 0 ? this.tickets.at(-1).id : 0;
+
         return id;
     }
 }
--- web/tickets/src/js/formulario.js
+++ web/tickets/src/js/formulario.js
@@ -2,17 +2,45 @@ import { Tickets } from "./Tickets";
 import { AssetsList } from "./assets-list-class";
 import { Asset } from "./asset-class";
 import { UsuarisList } from "./usuaris-list-class";
+import { ListTickets } from "./Tickets";
 
-var ticket = new Tickets;
 var asset = new Asset
 var listAsset = new AssetsList();
 var userList = new UsuarisList();
+//manejador 
+var ticketList = new ListTickets();
+//funcion que reciba la lista de ticket y esta se pone
+//dentro de un div y este div de destruye y contruye
+//para que este actualizada
+function listRefresh(list) {
+    console.log(list);
+    let oldList = document.getElementById('lista');
+    document.body.removeChild(oldList);
+    const newList = document.createElement('div');
+    newList.setAttribute('id', 'lista');
+    list.forEach(element => {
+        let line = document.createElement('div');
+        line.setAttribute('class', 'lista__line');
+        line.innerHTML = `${element.id} 
+        ${element.titulo}
+        ${element.descripcion}
+        ${element.assignedId}
+        ${element.assetId}
+        ${element.created}`;
+        newList.appendChild(line);
+
+
+
+    });
+    document.body.append(newList);
+
+}
 
 function usersOptions() {
     const users = userList.usuaris;
     let option = "";
     for (let i of users) {
-        option += `<option value =${i.id_usuari}>${i.username}</option>`;
+        option += `<option value =${i.id_usuari}> ${i.username} </option>`;
     }
     return option;
 }
@@ -32,23 +60,32 @@ function onSubmit() {
     const button = document.getElementById('addTicketButton');
     button.addEventListener("click", event => {
         event.preventDefault();
-        let ticketId = Tickets.getLastId() + 1;
+        //ya sabe el id que le voy a dar
+        let ticketId = ticketList.getLastId() + 1;
         let ticketName = document.getElementById('titulo').value;
         let ticketDesc = document.getElementById('description').value;
         let ticketAssigned = document.getElementById('tec').value;
         let ticketAsset = document.getElementById('assets').value;
-        var ticket = new Tickets(ticketId, ticketName, ticketDesc, ticketAssigned, ticketAsset);
-        console.log(ticketName);
+        const values = {
+            //ticketId: ticketId,
+            id: ticketId,
+            titulo: ticketName,
+            descripcion: ticketDesc,
+            assignedId: ticketAssigned,
+            assetId: ticketAsset
+        }
+        var ticket = new Tickets(values);
+        ticketList.postTicket(ticket);
+        console.log(ticketList.getLocalStorage);
+
+        console.log(ticket);
+        listRefresh(ticketList.getLocalStorage());
+
 
     })
-
-
-
 }
 
 export function creacionForm() {
-
-
 
     var html = `<form action="" >
 <label for="titulo">Tittle</label>
@@ -63,11 +100,17 @@ export function creacionForm() {
 </select>
 <button type="submit" id="addTicketButton">Crear Incidencia</button>
 <button type="reset" >Cancelar Incidencia</button>
-</form>`
+</form>
+
+`
     let div = document.createElement('div');
     div.innerHTML = html;
     document.body.append(div);
 
+    let list = document.createElement('div');
+    list.setAttribute('id', 'lista');
+    document.body.append(list);
+    listRefresh(ticketList.getLocalStorage());
     onSubmit();
 
 
