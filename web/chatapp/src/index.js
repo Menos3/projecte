603dc679da7179cf015a77877db3f874f2415506
Sergio Melado Alonso <ser_melado@hotmail.com>
2021-12-01 17:59:01 +0100

    continuacion chatapp

--- web/chatapp/src/index.js
+++ web/chatapp/src/index.js
@@ -1,4 +1,5 @@
 import {saludar} from './js/components'
+import "./styles.css";
 
 const nom = "Sergio"
 saludar(nom);
--- web/chatapp/webpack.config.js
+++ web/chatapp/webpack.config.js
@@ -1,14 +1,34 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 
     mode: 'development',
+    output: {
+        clean: true
+    },
     module: {
         
         rules: [
             {
                 test: /\.html$/,
                 loader: 'html-loader',
+                options: {
+                    minimize: true,
+                }
+            },
+            {
+                test: /\.css$/i,
+                exclude: /styles.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
             }
         ]
     },
@@ -16,6 +36,10 @@ module.exports = {
         new HtmlWebpackPlugin ({
             template: './src/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'nou-estil.css',
+            ignoreOrder: false
         }),
     ]
 }
