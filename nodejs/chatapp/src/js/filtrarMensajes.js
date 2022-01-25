c344e80323b49a2cb357b773059faf6d37645c09
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-01-25 16:44:14 +0100

    [M6] Filtraje de mensajes terminado

--- nodejs/chatapp/src/js/filtrarMensajes.js
+++ nodejs/chatapp/src/js/filtrarMensajes.js
@@ -36,120 +36,130 @@ export function crearHTMLFiltrarMensajes() {
 
     });
 
-}
 
-function crearListaMensajesSinFiltrar() {
 
-    //CREAR ELEMENT FORM
-    let formulario = document.createElement("form");
+    function crearListaMensajesSinFiltrar() {
 
-    //CREAR ELEMENT DIV
-    let divFiltrajeMensajes = document.createElement("div");
-    divFiltrajeMensajes.id = "divFiltrajeMensajes";
+        //CREAR ELEMENT DIV
+        let divMensajesSinFiltrar = document.createElement("div");
+        divMensajesSinFiltrar.id = "divMensajesSinFiltrar";
 
-    formulario.appendChild(divFiltrajeMensajes);
+        formulario.appendChild(divMensajesSinFiltrar);
 
-    //CREAR ELEMENT TABLE
-    let tablaSinFiltrar = document.createElement("table");
+        //CREAR ELEMENT TABLE
+        let tablaSinFiltrar = document.createElement("table");
 
-    //AÑADIR ELEMENT TABLE AL DIV
-    divFiltrajeMensajes.appendChild(tablaSinFiltrar);
+        //AÑADIR ELEMENT TABLE AL DIV
+        divMensajesSinFiltrar.appendChild(tablaSinFiltrar);
 
-    //A CADA VUELTA DE BUCLE, GENERA UNA FILA Y 5 COLUMNAS
-    for(let mensajes of listaMensajes.messageList) {
+        //A CADA VUELTA DE BUCLE, GENERA UNA FILA Y 5 COLUMNAS
+        for(let mensajes of listaMensajes.messageList) {
 
-        console.log("estoy dentro del bucle 1");
+            console.log("estoy dentro del bucle 1");
         
-        //CREAR ELEMENT TR
-        var tr = document.createElement("tr");
+            //CREAR ELEMENT TR
+            var tr = document.createElement("tr");
 
-        //CREAR ELEMENTS TD PARA LA ID, EL MENSAJE, PUBPRIV, CREATED Y PARA VISUALIZARLO
-        var tdId = document.createElement("td");
-        tdId.innerHtml = mensajes.id;
+            //CREAR ELEMENTS TD PARA LA ID, EL MENSAJE, PUBPRIV, CREATED Y PARA VISUALIZARLO
+            var tdId = document.createElement("td");
+            tdId.innerHtml = mensajes.id;
 
-        var tdMensaje = document.createElement("td");
-        tdMensaje.innerText = mensajes.message;
+            var tdMensaje = document.createElement("td");
+            tdMensaje.innerText = mensajes.message;
 
-        var tdFechaCreacion = document.createElement("td");
-        tdFechaCreacion.innerText = mensajes.created;
+            var tdFechaCreacion = document.createElement("td");
+            tdFechaCreacion.innerText = mensajes.created;
 
-        var tdPubPriv = document.createElement("td");
-        tdPubPriv.innerText = mensajes.pubpriv;
+            var tdPubPriv = document.createElement("td");
+            tdPubPriv.innerText = mensajes.pubpriv;
 
-        var tdVisualizar = document.createElement("td");
-        var btVisualizar = document.createElement("button");
-        btVisualizar.id = "btVisualizar";
+            var tdVisualizar = document.createElement("td");
+            var btVisualizar = document.createElement("button");
+            btVisualizar.id = "btVisualizar";
+            btVisualizar.innerHTML = "Visualitzar";
 
-        //AÑADIR ELEMENT A AL TDVISUALIZAR
-        tdVisualizar.appendChild(btVisualizar);
+            //AÑADIR ELEMENT A AL TDVISUALIZAR
+            tdVisualizar.appendChild(btVisualizar);
 
-        //AÑADIR LOS TD AL TR
-        tr.appendChild(tdId);
-        tr.appendChild(tdMensaje);
-        tr.appendChild(tdFechaCreacion);
-        tr.appendChild(tdPubPriv);
-        tr.appendChild(tdVisualizar);
+            //AÑADIR LOS TD AL TR
+            tr.appendChild(tdId);
+            tr.appendChild(tdMensaje);
+            tr.appendChild(tdFechaCreacion);
+            tr.appendChild(tdPubPriv);
+            tr.appendChild(tdVisualizar);
 
-        //AÑADIR EL TR A LA TABLE
-        tablaSinFiltrar.appendChild(tr);
+            //AÑADIR EL TR A LA TABLE
+            tablaSinFiltrar.appendChild(tr);
 
-        //EVENTLISTENER PARA VISUALIZAR EL MENSAJE
-        btVisualizar.addEventListener("click", (event) => {
+            //EVENTLISTENER PARA VISUALIZAR EL MENSAJE
+            btVisualizar.addEventListener("click", (event) => {
 
-            event.preventDefault();
-            alert("La id del missatge es: " + mensajes.id + " amb el missatge: " + mensajes.message + " creat el dia: " + mensajes.created + "i el missatge és: " + mensajes.pubpriv);
-        });
-        console.log("llegare aqui?");
+                event.preventDefault();
+                alert("La id del missatge es: " + mensajes.id + " amb el missatge: " + mensajes.message + " creat el dia: " + mensajes.created + " i el missatge és: " + mensajes.pubpriv);
+            });
+
+            console.log("llegare aqui?");
+        }
     }
-}
 
-function crearListaMensajesFiltrada(keyWord) {
+    function crearListaMensajesFiltrada(keyWord) {
 
-    //LISTA FILTRADA
-    var listaFiltrada = listaMensajes.filteredList(keyWord);
-    console.log(listaFiltrada);
+        //LISTA FILTRADA
+        var listaFiltrada = listaMensajes.filteredList(keyWord);
+        console.log(listaFiltrada);
+
+        let divMensajesFiltrados = document.createElement("div");
+        divMensajesFiltrados.id = "divMensajesFiltrados";
+
+        formulario.appendChild(divMensajesFiltrados);
+
+        let tablaFiltrada = document.createElement("table");
+
+        divMensajesFiltrados.appendChild(tablaFiltrada);
     
-    for(let mensajesFiltrados of listaFiltrada) {
+        for(let mensajesFiltrados of listaFiltrada) {
 
-        console.log("estoy dentro del bucle 2");
-        //CREAR ELEMENT TR
-        let tr = document.createElement("tr");
+            console.log("estoy dentro del bucle 2");
+            //CREAR ELEMENT TR
+            let tr = document.createElement("tr");
 
-        //CREAR ELEMENTS TD PARA LA ID, EL MENSAJE, PUBPRIV, CREATED Y PARA VISUALIZARLO
-        var tdId = document.createElement("td");
-        tdId.innerHtml = mensajesFiltrados.id;
+            //CREAR ELEMENTS TD PARA LA ID, EL MENSAJE, PUBPRIV, CREATED Y PARA VISUALIZARLO
+            var tdId = document.createElement("td");
+            tdId.innerHtml = mensajesFiltrados.id;
 
-        var tdMensaje = document.createElement("td");
-        tdMensaje.innerText = mensajesFiltrados.message;
+            var tdMensaje = document.createElement("td");
+            tdMensaje.innerText = mensajesFiltrados.message;
 
-        var tdFechaCreacion = document.createElement("td");
-        tdFechaCreacion.innerText = mensajesFiltrados.created;
+            var tdFechaCreacion = document.createElement("td");
+            tdFechaCreacion.innerText = mensajesFiltrados.created;
 
-        var tdPubPriv = document.createElement("td");
-        tdPubPriv.innerText = mensajesFiltrados.pubpriv;
+            var tdPubPriv = document.createElement("td");
+            tdPubPriv.innerText = mensajesFiltrados.pubpriv;
 
-        var tdVisualizar = document.createElement("td");
-        var btVisualizar = document.createElement("button");
-        btVisualizar.id = "btVisualizar";
+            var tdVisualizar = document.createElement("td");
+            var btVisualizar = document.createElement("button");
+            btVisualizar.id = "btVisualizar";
+            btVisualizar.innerHTML = "Visualitzar";
 
-        //AÑADIR ELEMENT A AL TDVISUALIZAR
-        tdVisualizar.appendChild(btVisualizar);
+            //AÑADIR ELEMENT A AL TDVISUALIZAR
+            tdVisualizar.appendChild(btVisualizar);
 
-        //AÑADIR LOS TD AL TR
-        tr.appendChild(tdId);
-        tr.appendChild(tdMensaje);
-        tr.appendChild(tdFechaCreacion);
-        tr.appendChild(tdPubPriv);
-        tr.appendChild(tdVisualizar);
+            //AÑADIR LOS TD AL TR
+            tr.appendChild(tdId);
+            tr.appendChild(tdMensaje);
+            tr.appendChild(tdFechaCreacion);
+            tr.appendChild(tdPubPriv);
+            tr.appendChild(tdVisualizar);
 
-        //AÑADIR EL TR A LA TABLE
-        tablaSinFiltrar.appendChild(tr);
+            //AÑADIR EL TR A LA TABLE
+            tablaFiltrada.appendChild(tr);
 
-        //EVENTLISTENER PARA VISUALIZAR EL MENSAJE
-        btVisualizar.addEventListener("click", (event) => {
+            //EVENTLISTENER PARA VISUALIZAR EL MENSAJE
+            btVisualizar.addEventListener("click", (event) => {
 
-            event.preventDefault();
-            alert("La id del missatge es: " + mensajesFiltrados.id + " amb el missatge: " + mensajesFiltrados.message + " creat el dia: " + mensajesFiltrados.created + "i el missatge és: " + mensajesFiltrados.pubpriv);
-        });
+                event.preventDefault();
+                alert("La id del missatge es: " + mensajesFiltrados.id + " amb el missatge: " + mensajesFiltrados.message + " creat el dia: " + mensajesFiltrados.created + "i el missatge és: " + mensajesFiltrados.pubpriv);
+            });
+        }
     }
 }
