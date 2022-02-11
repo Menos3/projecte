5e2faad6b58bf35b641a0bd3b59a5343277e2668
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-02-11 13:48:06 +0100

    [M7] modelo y migracion completadas

---
+++ laravel/database/migrations/2022_02_11_121749_create_chats_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->integer('author_id');
+            $table->timestamps('created');
+        });
+
+        Schema::table('chats', function(Blueprint $table) {
+            $table->foreign('author_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+        Schema::table('chat', function(Blueprint $table) {
+            $table->dropForeign('author_id_foreign');
+        });
+        Schema::dropIfExists('chats');
+    }
+}
