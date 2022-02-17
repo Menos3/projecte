f7b3fd319cdd5762d5008924d374bd883417597f
cristina <cristinaaran89@gmail.com>
2022-02-17 17:03:22 +0100

    recien horneadito, listo para servir grrrr

--- design/layaout_grid_mapaCris.js
+++ design/layaout_grid_mapaCris.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([41.2311566, 1.7285224886456212], 10);
+var map = L.map('map').setView([41.2311566, 1.7285224886456212], 24);
 // map.panto({ lat: 41.25862, long: 1.77086 });
 var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
@@ -13,15 +13,15 @@ let marker = L.marker([41.2311566, 1.7285224886456212]).addTo(map);
 
 // var mapita=L.map.locate({ setView: true, maxZoom: 10 });
 
-// function onLocationFound(e) { 
-//     var radius = e.accuracy / 2;
-//     let markus = L.marker(e.latlng).addTo(map).bindPoppup("Esta Aqui!", radius).openPopup();
-//     console.log(markus);
-//     L.circle(e.latlng, radius).addTo(map);
+function onLocationFound(e) { 
+    var radius = e.accuracy / 2;
+    let markus = L.marker(e.latlng).addTo(map).bindPoppup("Esta Aqui!", radius).openPopup();
+    console.log(markus);
+    L.circle(e.latlng, radius).addTo(map);
     
-// }
+}
 
-// function onLocationError(e) { alert(e.message); }
+function onLocationError(e) { alert(e.message); }
 var x = document.getElementById("map");
 
 function getLocation() {
@@ -31,14 +31,30 @@ function getLocation() {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
+// var latitud = 41.30;
+// var longitud =1.7285224886456212;
 
 function showPosition(position) {
-    var latitud = position.coords.latitude;
-    var longitud = position.coords.longitude;
-    
-    var mapi = L.map('map').setView([latitud, longitud], 10);
-    var radius = e.accuracy / 2;
-    L.marker(e.latlng).addTo(mapi).bindPoppup("Esta Aqui!", radius).openPopup();
-    
+  
+  var latitud = position.coords.latitude;
+  var longitud = position.coords.longitude;
+  
+  map.setView([latitud, longitud], 10);
+  let marker = L.marker([latitud, longitud]).addTo(map);
+  marker.bindPopup("Esta Aqui!").openPopup();
+  
 
 }
+// L.Routing.control({
+//   waypoints: [
+//       L.latLng(41.2311566, 1.7285224886456212),
+//       L.latLng(latitud, longitud),
+//   ],
+//   routeWhileDragging: true
+// }).addTo(map);
+  // var latitud = position.coords.latitude;
+  // var longitud = position.coords.longitude;
+  
+
+
+getLocation();
