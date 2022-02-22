8a2769d12e82ae8ec12da5cf869690557b740a6a
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-02-22 20:15:58 +0100

    [M9] accesskey

--- design/layaout_grid_mapaSergio.js
+++ design/layaout_grid_mapaSergio.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([41.2311566, 1.7285224886456212],24);
-// map.panto({ lat: 41.25862, long: 1.77086 });
+
 var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -10,8 +10,6 @@ var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}
 }).addTo(map);
 
 let marker = L.marker([41.2311566, 1.7285224886456212]).addTo(map);
-
-// var mapita=L.map.locate({ setView: true, maxZoom: 10 });
 
 function onLocationFound(e) { 
     var radius = e.accuracy / 2;
@@ -31,8 +29,6 @@ function getLocation() {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
-// var latitud = 41.30;
-// var longitud =1.7285224886456212;
 
 function showPosition(position) {
   
@@ -42,19 +38,22 @@ function showPosition(position) {
   map.setView([latitud, longitud], 24);
   let marker = L.marker([latitud, longitud]).addTo(map);
   marker.bindPopup("Esta Aqui!").openPopup();
-  
 
 }
-// L.Routing.control({
-//   waypoints: [
-//       L.latLng(41.2311566, 1.7285224886456212),
-//       L.latLng(latitud, longitud),
-//   ],
-//   routeWhileDragging: true
-// }).addTo(map);
-  // var latitud = position.coords.latitude;
-  // var longitud = position.coords.longitude;
-  
-
 
 getLocation();
+
+//ACCESSKEYS
+key('ctrl+alt+g', function(){alert(navigator.geolocation.getCurrentPosition(showPosition))});
+key('ctrl+alt+c', function(){
+  
+  var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1
+  }).addTo(map);
+
+});
