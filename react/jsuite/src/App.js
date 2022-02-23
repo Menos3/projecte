31322831e28c49680aebd72282ef1990d4d341b2
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-02-23 20:52:35 +0100

    [M6] preparacion react

--- react/jsuite/src/App.js
+++ react/jsuite/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Body from './Body'
+import Footer from './Footer'
+import Header from './Header'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <div><Header/></div>
+    <div><Body/></div>
+    <div><Footer/></div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ react/jsuite/src/chatapp/Chatapp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chatapp = () => {
+  return (
+    <div>Chatapp</div>
+  )
+}
+
+export default Chatapp
