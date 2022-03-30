78495b145cfdf082229d1e3344fb384b2f129e63
cristina <cristinaaran89@gmail.com>
2022-03-30 11:15:06 +0200

    para subir

--- react/jsuite/src/chatapp/Chatapp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Chatapp = () => {
-  return (
-    <div>Chatapp</div>
-  )
-}
-
-export default Chatapp
--- react/jsuite/src/proyecte/Commons/Body.js
+++ react/jsuite/src/proyecte/Commons/Body.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import ListaActores from '../../Ejercicio1/ListaActores'
+
 import ListTickets from '../ListTickets'
 import Acciones from './Acciones'
 import Navegacion from './Navegacion'
-import Calculadora from '../../Ejericio2/Calculadora'
+
 
 const Body = () => {
   return (
-    <>Body
+    <>
       {/* <ListaActores/> */}
       {/* <Navegacion/> */}
       {/* <Acciones /> */}
