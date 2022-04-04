32a3396b01cf662404e8d1edc28ee89c0808584a
cristina <cristinaaran89@gmail.com>
2022-04-04 15:21:13 +0200

    a ve

--- react/jsuite/src/App.js
+++ react/jsuite/src/App.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Login from './Login'
 import Chatapp from './chatapp/Chatapp'
 import GestorTickets from './proyecte/Commons/GestorTickets'
 import { Routes, Route } from "react-router-dom";
 import Home from './Home'
+import { UserContext } from './UseContextUser';
+
 
 
 const App = () => {
+  const estado=useState("cristina")
+  const [usuario, setUsuario] = estado;
+
   return (
+    <UserContext.Provider value={{usuario:usuario, setUsuario:setUsuario}}>
     <Routes>
-      <Route path="/" element={<Home />} >
-          <Route path="/" element={<GestorTickets />}/>
-          <Route  path="/tickets" element={<GestorTickets />}/>
-          <Route path="/chatapp" element={<Chatapp />} />
+        <Route path="/" element={<Home estado={ estado} />} >
+          <Route path="/" element={<GestorTickets estado={ estado}/>}/>
+          <Route  path="/tickets" element={<GestorTickets estado={ estado} />}/>
+          <Route path="/chatapp" element={<Chatapp estado={ estado} />} />
+          <Route path="/login" element={<Login estado={ estado}/>} />
+          
         
         </Route>
       </Routes>
+    </UserContext.Provider>
   )
 }
 
--- react/jsuite/src/Home.js
+++ react/jsuite/src/Home.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Button} from 'react-bootstrap'
 import { Link, Outlet } from 'react-router-dom'
+import { UserContext} from './UseContextUser'
 
 export default function Home() {
+  const estado = useContext(UserContext)
+  const { usuario, setUsuario}= estado
+
   return (
     <div>BIENVENIDOSSSSSSSSSSSSSSSSSSSSSSSSSSSSs
 
-      <Link to="/tickets" ><big>Tickets</big></Link>
-      <Link to="/chatapp" ><big>ChatApp</big></Link>
+      <Button  variant="outline-primary"><Link to="/tickets" >Tickets</Link></Button>
+  
+
+      <Button  variant="outline-primary"><Link to="/chatapp" >ChatApp</Link></Button>
+      
+      <Button  variant="outline-primary"><Link to="/login" >Login</Link></Button>
+
       <hr></hr>
+
+      <div>{ usuario}</div>
       <Outlet/>
 
           
