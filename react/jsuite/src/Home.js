425432901a1519b4559ddb9681cc85f52b0fe1f9
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-04-08 16:15:13 +0200

    [M6] tabulacion

--- react/jsuite/src/Home.js
+++ react/jsuite/src/Home.js
@@ -5,14 +5,12 @@ import { UserContext } from './UseContextUser'
 
 export default function Home() {
   const estado = useContext(UserContext)
-  const { usuario, setUsuario}= estado
+  const { usuario, setUsuario }= estado
 
   return (
     <div>
       <div>
         <Button  variant="outline-primary"><Link to="/tickets" >Tickets</Link></Button>
-    
-
         <Button  variant="outline-primary"><Link to="/chatapp" >ChatApp</Link></Button>
         {!usuario ? (
           <>
@@ -29,7 +27,7 @@ export default function Home() {
 
         <hr></hr>
 
-        <div>{ usuario}</div>
+        <div>{ usuario }</div>
         <Outlet/>
       </div>
       <h1>Bienvenidos a nuestra APP</h1>
